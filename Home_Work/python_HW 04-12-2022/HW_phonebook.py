46239af86c54a243061419b6c753285776061ef6
Kyle Huskey <kylehuskey@gmail.com>
2022-04-12 21:38:52 -0400

    Updated Home work

--- Home_Work/python_HW 04-12-2022/HW_phonebook.py
+++ Home_Work/python_HW 04-12-2022/HW_phonebook.py
@@ -10,14 +10,15 @@ while True:
 What would you like to do? Please select(1-5) """))
 
     if user_input == 1:
-        person = input('Who are you looking for? ')
+        person = input("""Who are you looking for?\nName: """)
         for contact in phone_book:
             if person == contact['Name']:
                 i = phone_book.index(contact)
-                print(phone_book[i])
+                num_pulled = contact['Phone Number']
+                print(f"""Found entry for {person}: {num_pulled}""")
     elif user_input == 2:
         new_contact_name = input('Ok, what is the name of the contact you want saved? ')
-        new_contact_num = (input('And what is their number? '))
+        new_contact_num = input('And what is their number? ')
         new_contact = {'Name': new_contact_name, 'Phone Number': new_contact_num}
         phone_book.append(new_contact)
         print(f'Contact info for {new_contact_name} stored.')
@@ -29,8 +30,8 @@ What would you like to do? Please select(1-5) """))
                 del phone_book[i]
         print(f'Contact info for {deleted_contact} is now deleted.')
     elif user_input == 4:
-        for i in phone_book:
-            print(i)
+        for contact in phone_book:
+            print("Name: " + contact['Name'] + " " "\nPhone Number: " + contact['Phone Number'])
     elif user_input == 5:
         print('Bye for now!')
         break
